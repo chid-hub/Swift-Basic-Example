e8d278f126fd6ca5330223971acbd6e19e88ef77
Chidhambaram <chidhambaramcse@gmail.com>
2021-08-13 08:10:52 +0530

    Login Screen and function

--- MyFirstProject/MyFirstProject/ViewController.swift
+++ MyFirstProject/MyFirstProject/ViewController.swift
@@ -9,12 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("")
     }
 
+    @IBAction func signInAction(_ sender: Any) {
+       
+       let username = self.userNameTextField.text
+        let password = self.passwordTextField.text
 
+        if username == "User" && password == "1234" {
+            
+            print("You are successfully logged in")
+        }else{
+            print("Useraname or password is worng")
+        }
+        
+        print("signInAction clicked")
+    }
+    
 }
 
